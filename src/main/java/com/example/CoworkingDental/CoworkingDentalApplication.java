87ea4a4e8c940f6349bfbedaada1f95fd17d7c75
angel martin <angelmmgc@gmail.com>
2022-11-05 19:46:49 +0100

    subiendo carpetas

---
+++ src/main/java/com/example/CoworkingDental/CoworkingDentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoworkingDental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingDentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingDentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CoworkingDental/CoworkingDentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CoworkingDental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingDentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
